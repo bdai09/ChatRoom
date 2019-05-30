81cb3d77f7f1a3217f697915cccf52c4002a92b4
bo dai <bdai9@myseneca.ca>
2019-05-30 14:47:06 -0400

    server

--- signalingServer.js
+++ signalingServer.js
@@ -10,3 +10,21 @@ const cfg={
     ssl_key: './server.key',
     ssl_cert:'./server.crt'
 };
+ //TYPES supported by mediarecorder
+const mimeTypes={
+    '.js':'text/javascript',
+    '.html':'text/html',
+    '.css':'text/css',
+}
+let connectionArray=[];
+//store the mapping relationship between connection and ID
+const connIDMap={};
+//store the mapping relationship between connection and username
+const connUserMap={};
+
+let nextID=Date.now();
+let appendToMakeUnique=1;
+
+const processRequest=(req,res)=>{
+    const lookup=
+}
